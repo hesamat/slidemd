8d17f4861e79639f8ccbec539d4f528587905766
hesamat <hesam.alizadeh@gmail.com>
2026-07-26 22:20:08 -0700

    \ feat: add Set as Background option to image properties panel\

--- src/editor/core/edit-controller.js
+++ src/editor/core/edit-controller.js
@@ -499,6 +499,7 @@ export class EditController {
         this.markdownEditor?.setValue(updated, { suppressOnChange: true });
         this.unsavedMarkdown.set(this.currentSlideIndex, updated);
         this.updateUnsavedChangesFlag();
+        this.previewUpdater.update();
       },
     );
 
